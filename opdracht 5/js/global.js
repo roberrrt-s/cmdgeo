308cb2ccf42a2da0a5bb0466f48aa3809efe5496
roberrrt-s <robert.spier@gmail.com>
2016-02-02 21:49:30 +0100

    initial commit for opdracht 5

---
+++ opdracht 5/js/global.js
@@ -0,0 +1,61 @@
+// IIFE to avoid global variables
+(function() {
+	// Strict mode at the top of this function to enable this for the entire document
+	"use strict"
+
+// Declaring the application object
+var app = {
+	init: function() {
+		// Initializing application by calling the routes object.
+		console.log("Initialize application")
+		routes.init();
+	}
+}
+
+// Declaring the routes object
+var routes = {
+	init: function() {
+		console.log("Initialize routes")
+
+		// Adding an event listener to the window, which triggers when the hash (#) changes.
+		// it invokes an anonymous function that passes to the sections object to toggle between the elements, using the window's hash as a parameter
+		window.addEventListener("hashchange", function() {
+			sections.toggle(window.location.hash)
+		})
+
+		// We have to check if the user comes from a place that already included a hash!
+		sections.toggle(window.location.hash)
+
+	}
+}
+
+// Declaring the sections object
+var sections = {
+	toggle: function(route) {
+		console.log("Toggle between the sections, displaying:" + route)
+
+		// Grabbing all sections in a variable
+		var a = document.getElementsByTagName("section")
+
+		// Looping through all routes, while skipping the first one, the navigation by starting i at 1
+		for (var i = 1; i < a.length; i++) {
+
+			// Checking if the id of the section corresponds with the route parameter (minus the #)
+		    if(a[i].id === route.substr(1)) {
+		    	a[i].style.display = "block";
+		    }
+		    // Else, remove the visibility
+		    else {
+		    	a[i].style.display = "none";
+		    }
+		}
+
+
+
+	}
+}
+
+app.init();
+
+}());
+
